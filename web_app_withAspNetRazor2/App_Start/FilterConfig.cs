e77e5a13de20cec3f9e2e51150162e35203e835a
Waseem Soomro <ws@waseemsoomro.com>
2016-03-29 10:47:21 +0200

    some changes in controller
    comments added, and new controller "Customers" added

--- web_app_withAspNetRazor2/App_Start/FilterConfig.cs
+++ web_app_withAspNetRazor2/App_Start/FilterConfig.cs
@@ -5,7 +5,7 @@ namespace web_app_withAspNetRazor2
 {
     public class FilterConfig
     {
-        
+        // error handling
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
--- web_app_withAspNetRazor2/Controllers/HomeController.cs
+++ web_app_withAspNetRazor2/Controllers/HomeController.cs
@@ -6,8 +6,14 @@ using System.Web.Mvc;
 
 namespace web_app_withAspNetRazor2.Controllers
 {
+    // The name of the controller is Home but it's created as HomeController.
+    // it's used in RouteConfig.cs as Home
     public class HomeController : Controller
     {
+        // The name of Action is FunctionName
+        // /home/index/5    for. e.g. action "index" , id '5'
+
+        // "ActionResult" is the view type which will return to view.
         public ActionResult Index()
         {
             return View();
